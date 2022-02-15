4cd01f98d3cffed6565b9082bbe8c83e9645a71d
petersandqvist <petersandqvist_81@hotmail.com>
2022-02-15 15:42:27 +0100

    changes

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -7,7 +7,7 @@ export const Search = () => {
   const [name, setName] = useState('');
 
   //when useLazyQuery is called it returns a function that can trigger the query manually, for exampel on an event like a button click
-  const [getCharactersByName, { error, data, loading, called }] = useLazyQuery(
+  const [getCharactersByName, { error, data, loading, refetch }] = useLazyQuery(
     GET_CHARACTER_BY_NAME,
     {
       variables: {
@@ -16,45 +16,51 @@ export const Search = () => {
     }
   );
 
-  // TODO create button that reset searchresult
-
   const onChange = (e: { target: { value: React.SetStateAction<string> } }) => {
     setName(e.target.value);
+    //not sure if refetch is correct here
+    refetch();
   };
 
   const handleKeyPress = (e: { keyCode: number }) => {
     if (e.keyCode === 13) {
-      getCharactersByName();
     }
+  };
+
+  const resetInput = () => {
+    setName('');
+    getCharactersByName();
   };
 
   return (
     <div className='form-wrapper'>
-      <div className='inputs'>
-        <h1>Search for a name, like Rick, Morty etc 🚀</h1>
-        <input
-          autoFocus
-          className='search-input'
-          value={name}
-          onChange={onChange}
-          onKeyDown={handleKeyPress}
-          placeholder=''
-        ></input>
-        <button onClick={() => getCharactersByName()}>Search</button>
-      </div>
-      {loading && <div>loading...</div>}
+      <form onChange={() => getCharactersByName({ variables: { name: name } })}>
+        <div className='inputs'>
+          <h1>Search for a name, like Rick, Morty etc 🚀</h1>
+          <input
+            autoFocus
+            className='search-input'
+            value={name}
+            onChange={onChange}
+            onKeyDown={handleKeyPress}
+            placeholder=''
+          ></input>
+          <button onClick={() => resetInput()}>Clear</button>
+        </div>
+      </form>
+      {loading && <div className='loading'>loading...</div>}
       {error && <div>Something went wrong</div>}
       <ul>
         {data &&
-          data.characters.results.map(
+          data?.characters?.results.map(
             (character: {
               image: string | undefined;
               id: string | undefined;
               name: React.ReactNode;
             }) => {
               return (
-                <li>
-                  <Link to={`/${character.id}`} key={character.id}>
+                <li key={character.id}>
+                  <Link to={`/${character.id}`}>
                     <img src={character.image} alt='RickAndMorty' />
                     <h2>{character.name}</h2>
                   </Link>
